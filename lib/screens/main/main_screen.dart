00327f676f94dcdec2f7847ca912602ea33402db
Hoover Jonatan Apaza Blanco <hoover_apaza@hotmail.com>
2022-10-19 20:43:11 -0400

    main screen
    dar funcionalidad a botones

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -18,6 +18,17 @@ class _MainScreenState extends State<MainScreen> {
         title: Center(child: Text('Bienvenido!')),
         automaticallyImplyLeading: false,
         backgroundColor: Colors.black26,
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        }),
       ),
       drawer: Sidenav(),
     );
@@ -31,49 +42,60 @@ class Sidenav extends StatelessWidget {
       child: ListView(
         children: [
           Container(
-            height: 400,
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.start, children: [
               Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    'Instituto Tecnico Cepess',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  )),
+                padding: EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/logo/logocepess.png',
+                  width: 110,
+                  height: 110,
+                ),
+              ),
               Divider(
                 color: Colors.grey.shade400,
-              ),
-              Icon(
-                Icons.person_pin_outlined,
-                size: 80,
               ),
               const SizedBox(
                 height: 20,
               ),
-              Text(
-                'Apaza Blanco Hoover Jonatan',
-              ),
+              Text('Apaza Blanco Hoover Jonatan'),
               const SizedBox(
                 height: 10,
               ),
-              Text('Activo'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.circle,
+                    color: Colors.green,
+                    size: 10,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text('Activo')
+                ],
+              ),
               Divider(
                 color: Colors.grey.shade400,
               ),
-              ListTile(
-                title: Text('Consulta de Notas'),
-                leading: Icon(Icons.task_outlined),
+              buidMenuItem(text: 'Consulta de notas', icon: Icons.list),
+              Divider(
+                color: Colors.grey.shade400,
               ),
-              ListTile(
-                title: Text('Cerrar Sesion'),
-                leading: Icon(Icons.logout),
-              )
+              buidMenuItem(text: 'Cerrar Sesion', icon: Icons.logout)
             ]),
           ),
         ],
       ),
     );
   }
+}
+
+Widget buidMenuItem({required String text, required IconData icon}) {
+  return ListTile(
+    leading: Icon(icon),
+    title: Text(text),
+    onTap: () {},
+  );
 }
